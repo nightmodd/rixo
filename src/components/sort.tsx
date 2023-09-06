bc3009aa590a3f6f3d3f76cb8f8183a3364080ff
Mostafa Esmail <mostafaghareeb132@gmail.com>
2023-09-06 10:56:40 +0300

    fix: fix bug for build
    Signed-off-by: Mostafa Esmail <mostafaghareeb132@gmail.com>

--- src/components/sort.tsx
+++ src/components/sort.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 import { SORT_LABELS, SORT_OPTIONS } from '../constants/sort';
 
