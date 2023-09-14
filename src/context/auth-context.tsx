44ed44d8f7e15ae320ae1a73e7e1743e05ef482b
Mostafa Esmail <mostafaghareeb132@gmail.com>
2023-09-14 13:35:00 +0300

    feat: added the login and register optimized logic
    Signed-off-by: Mostafa Esmail <mostafaghareeb132@gmail.com>

---
+++ src/context/auth-context.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+
+import { auth } from '../config/firebase-config';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  fetchSignInMethodsForEmail,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+const AuthContext = React.createContext({
+  currentUser: null,
+  signup: (email: string, password: string): Promise<any> => {
+    console.log(email + '\n' + password);
+    return Promise.resolve();
+  },
+  signin: (email: string, password: string): Promise<any> => {
+    console.log(email + '\n' + password);
+    return Promise.resolve();
+  },
+  logout: () => {
+    console.log('signout');
+  },
+  googleSignIn: (): Promise<void> => {
+    return Promise.resolve();
+  },
+});
+
+export default AuthContext;
+
+interface AuthContextProviderProps {
+  children: React.ReactNode;
+}
+export const AuthContextProvider = (props: AuthContextProviderProps) => {
+  const [currentUser, setCurrentUser] = useState<any>('');
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user.email);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [currentUser]);
+
+  const googleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider)
+      .then(async (result) => {
+        const user = result.user;
+        if (user) {
+          setCurrentUser(user.email);
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  const checkEmailExists = async (email: string) => {
+    const signInMethods = await fetchSignInMethodsForEmail(auth, email);
+    return signInMethods.length > 0;
+  };
+
+  const signup = async (email: string, password: string) => {
+    try {
+      const emailExists = await checkEmailExists(email);
+      if (emailExists) {
+        alert('This email already exists');
+        throw TypeError('This email already exists');
+      }
+      return createUserWithEmailAndPassword(auth, email, password);
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  const signin = (email: string, password: string) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logout = async () => {
+    return signOut(auth);
+  };
+
+  const value = {
+    currentUser,
+    signup,
+    signin,
+    logout,
+    googleSignIn,
+  };
+  return (
+    <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>
+  );
+};
