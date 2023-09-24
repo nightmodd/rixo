ceeb6463b4e6b712996c91bd654c96d103e82b39
Mostafa Esmail <mostafaghareeb132@gmail.com>
2023-09-24 06:26:38 +0300

    feat: added the avilable sections component
    Signed-off-by: Mostafa Esmail <mostafaghareeb132@gmail.com>

--- src/components/home-components/avilable-sections.tsx
+++ src/components/home-components/avilable-sections.tsx
@@ -14,27 +14,49 @@ const AvilableSections = (props: AvilableSectionsProps) => {
   const sections = props.sections;
 
   return (
-    <Swiper
-      className={styles.avilable_sections}
-      breakpoints={{
-        1000: {
-          slidesPerView: 3,
-        },
-      }}
-      slidesPerView={4}
-      navigation={true}
-      spaceBetween={35}
-      modules={[Navigation, Pagination]}
-    >
-      {sections.map((section: HomeSection, index: number) => (
-        <SwiperSlide key={index} className={styles.section_card}>
-          <Link to={`/collections/${section.id}`}>
-            <img src={section.img} alt={section.description} data-id={index} />
-            <span>{section.title}</span>
-          </Link>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className={styles.sections_container}>
+      <span className={styles.title}>Styles Inspired by Vintage</span>
+      <div className={styles.avilable_sections}>
+        <Swiper
+          breakpoints={{
+            1000: {
+              slidesPerView: 4,
+              loop: false,
+            },
+            700: {
+              slidesPerView: 3,
+              loop: false,
+            },
+          }}
+          slidesPerView={2}
+          loop={true}
+          pagination={{
+            type: 'progressbar',
+          }}
+          navigation={true}
+          spaceBetween={25}
+          modules={[Navigation, Pagination]}
+        >
+          {sections.map((section: HomeSection, index: number) => (
+            <SwiperSlide key={index}>
+              <div className={styles.section_card}>
+                <Link to={`/collections/${section.id}`}>
+                  <img
+                    src={section.img}
+                    alt={section.description}
+                    data-id={index}
+                  />
+                  <div className={styles.card_bottom}>
+                    <span>{section.title}</span>
+                    <i className="fa-solid fa-arrow-right-long"></i>
+                  </div>
+                </Link>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
   );
 };
 
