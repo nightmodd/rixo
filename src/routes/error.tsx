15903ff39ef8a7660f620aa1d0ca20dc334f5027
Mostafa Esmail <mostafaghareeb132@gmail.com>
2023-09-27 10:14:52 +0300

    Resolve conflict : resolve conflict with main branch
    Signed-off-by: Mostafa Esmail <mostafaghareeb132@gmail.com>

--- src/routes/error.tsx
+++ src/routes/error.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import MainNavigation from '../components/navbar-components/navbar';
 
 import styles from './error.module.scss';
 
-type pageContentProps = {
-  title: string;
-  children: React.ReactNode;
-};
-const PageContent: React.FC<pageContentProps> = (props) => {
-  const { title, children } = props;
+const PageContent: React.FC = () => {
+  const title = '<NOT_FOUND>';
   return (
     <div className={styles.content}>
-      <h1>{title}</h1>
-      {children}
+      <img
+        src="https://d33wubrfki0l68.cloudfront.net/4bb452ab9f63c172a3c418d733495bf7e6394b9e/ff073/doge_sad.jpg"
+        alt="doge sad"
+      />
+      <span>{title}</span>
+      <p>You just hit a route that doesn't exist... the sadness.</p>
+      <Link to={'/'}>Go home</Link>
     </div>
   );
 };
 
 const ErrorPage = () => {
-  const title = 'An Error Occurred';
-  const errorMessage = 'Something went wrong!';
-
   return (
     <>
       <MainNavigation />
-      <PageContent title={title}>
-        <p>{errorMessage}</p>
-      </PageContent>
+      <PageContent></PageContent>
     </>
   );
 };
